5473131c90371f54cfe6e8e0a2cbf252a42706d3
mtojar <mtojar@gmail.com>
2015-08-10 12:54:36 +0200

    busqueda de una pregunta 1.0

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,6 +14,34 @@ exports.load = function(req, res, next, quizId)
 		  	).catch(function(error){ next(error);});
 		  };
 
+//GET /quizes
+exports.index=function(req, res) 
+          {
+          	if(req.query.search)
+          	{
+          		//res.render('quizes/tem.ejs', {search: req.query.search});
+          		models.Quiz.findAll({
+          				where: ["pregunta like ?", "%"+req.query.search+"%"]
+          			}).then(function(quizes)
+          					{
+          						if(quizes.length != 0)
+          							res.render('quizes/index', {quizes:quizes});
+          						else 
+          						{
+          							res.render('quizes/mensajes', {
+          										estilo:"color:#F00; font-size:12px; font-style:italic;text-transform:capitalize", 
+          										mensaje:"No existen consultas con el dato "+req.query.search});
+          						}
+          					}).catch(function(error){ next(error); } );
+          	} else
+          	{
+            	models.Quiz.findAll().then(function(quizes)
+                	{
+                    	res.render('quizes/index.ejs', {quizes:quizes} );
+                  	}).catch(function(error){ next(error); })
+        	}
+          };
+
 //GET /quizes/:id
 //ya hemos preguntado por la pregunta en load
 exports.show = function(req, res)
@@ -32,14 +60,6 @@ exports.answer = function(req, res)
 			res.render('quizes/answer', {quiz:req.quiz, respuesta: resultado});
           };          
 
-//GET /quizes
-exports.index=function(req, res) 
-          {
-            models.Quiz.findAll().then(function(quizes)
-                  {
-                    res.render('quizes/index.ejs', {quizes:quizes} );
-                  }).catch(function(error){ next(error); })
-          };
 
 //GET /quizes/author
 exports.authors = function(req, res)
